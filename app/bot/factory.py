0e414ee783aa2e4938e5dcf257e5531fbe3860d0
asya5555 <plotnikovaolesya@gmail.com>
2026-07-18 20:16:29 +0300

    Add app/bot/factory.py

---
+++ app/bot/factory.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from app.bot.handlers import router
+from app.core.config import Settings
+
+
+def create_bot(settings: Settings) -> Bot:
+    return Bot(
+        token=settings.telegram_bot_token,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+    )
+
+
+def create_dispatcher() -> Dispatcher:
+    dispatcher = Dispatcher(storage=MemoryStorage())
+    dispatcher.include_router(router)
+    return dispatcher
